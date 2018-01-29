1705a91d376d9d89aa2f6cb4de82fd17db0dbcca
Morgan Mays <momoney1157@gmail.com>
2018-01-29 13:29:46 -0600

    Initial Commit

---
+++ ShelterFinder USER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShelterFinder USER
+//
+//  Created by Morgan Mays on 1/29/18.
+//  Copyright © 2018 Morgan Mays. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
